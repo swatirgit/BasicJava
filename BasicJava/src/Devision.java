44280194e2c990364fc52c6067b34e9da2f7f455
swati <swati@192.168.1.101>
2020-10-07 17:29:00 +0530

    Second comit

---
+++ BasicJava/src/Devision.java
@@ -0,0 +1,17 @@
+
+public class Devision {
+	
+	public static void main(String[] args) {
+		
+		System.out.println(Devision.div());
+	}
+
+	public static double div() {
+		
+	double a=10.5;
+	double b=5;
+	double result=a/b;
+	
+	return result;
+	}
+}
